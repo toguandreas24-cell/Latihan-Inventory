f63273c8fa836a618208bcb8944941e9489d5b13
toguandreas24-cell <toguandreas24@gmail.com>
2026-01-22 22:11:32 +0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = require("../config/database");
-
-const Item = require("./item")(sequelize, DataTypes);
-
-module.exports = {
-  sequelize,
-  Item
-};
